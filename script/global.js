7df5018203e231925e3ed826b634a774a5922bdd
moying328 <2398437082@qq.com>
2021-01-21 22:09:52 +0800

    使用纯css代替js

--- script/global.js
+++ script/global.js
@@ -1,11 +1,11 @@
 window.onload=function(){
-  const btn=document.getElementById("modeBtn");
-    btn.addEventListener('click',(e)=>{
-      const body=document.body;
-      if(body.className !== 'night'){
-        body.className='night';
-      }else{
-        body.className='';
-      }
-    })
+  // const btn=document.getElementById("modeBtn");
+  //   btn.addEventListener('click',(e)=>{
+  //     const body=document.body;
+  //     if(body.className !== 'night'){
+  //       body.className='night';
+  //     }else{
+  //       body.className='';
+  //     }
+  //   })
 }
