5d425106af2ba2b604fa2797f6d58bd06dc7804d
moying328 <2398437082@qq.com>
2021-01-21 21:53:36 +0800

    js样式抽取，只负责状态切换

--- script/global.js
+++ script/global.js
@@ -2,14 +2,10 @@ window.onload=function(){
   const btn=document.getElementById("modeBtn");
     btn.addEventListener('click',(e)=>{
       const body=document.body;
-      if(e.target.innerHTML==='🌞'){
-        body.style.backgroundColor='black';
-        body.style.color='white';
-        e.target.innerHTML = '🌜';
+      if(body.className !== 'night'){
+        body.className='night';
       }else{
-        body.style.backgroundColor = 'white';
-        body.style.color = 'black';
-        e.target.innerHTML = '🌞';
+        body.className='';
       }
     })
 }
