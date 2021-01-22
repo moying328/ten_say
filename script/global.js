f1fabe19275b220f3afce720f2f22480b44a093f
moying <moying.wu@gzgi.com>
2021-01-22 15:51:50 +0800

    数据抽象和去副作用化

--- script/global.js
+++ script/global.js
@@ -11,15 +11,23 @@ window.onload=function(){
   
   // three
   const traffic=document.querySelector('.traffic');
-  function loop(subject){
-    subject.className='traffic pass';
-    setTimeout(()=>{
-      subject.className='traffic wait';
-      setTimeout(()=>{
-        subject.className='traffic stop';
-        setTimeout(loop.bind(null, subject), 3500);
-      },1500)
-    },5000)
+  function signalLoop(subject,signals=[],onSignal){
+    const signalCount=signals.length;
+    function updateState(i){
+      const {signal,duration}=signals[i % signalCount];
+      // subject.className=signal;
+      onSignal(subject,signal);
+      setTimeout(updateState.bind(null,i+1),duration)
+    }
+    updateState(0)
   }
-  loop(traffic)
+  // 数据抽象
+  const signals = [
+    {signal: 'pass', duration: 5000},
+    {signal: 'wait', duration: 3500},
+    {signal: 'stop', duration: 1500},
+  ];
+  signalLoop(traffic, signals,(subject,signal)=>{
+    subject.className=`traffic ${signal}`
+  });
 }
