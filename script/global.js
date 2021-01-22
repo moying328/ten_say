3e17237868067e91e4cf171c10b70af2b5a18e73
moying <moying.wu@gzgi.com>
2021-01-22 14:58:17 +0800

    函数体内部不应该有完全来自外部环境的变量，除非这个函数不打算复用

--- script/global.js
+++ script/global.js
@@ -8,4 +8,18 @@ window.onload=function(){
   //       body.className='';
   //     }
   //   })
+  
+  // three
+  const traffic=document.querySelector('.traffic');
+  function loop(subject){
+    subject.className='traffic pass';
+    setTimeout(()=>{
+      subject.className='traffic wait';
+      setTimeout(()=>{
+        subject.className='traffic stop';
+        setTimeout(loop.bind(null, subject), 3500);
+      },1500)
+    },5000)
+  }
+  loop(traffic)
 }
